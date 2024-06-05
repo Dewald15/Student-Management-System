7ff75717e192fc5cffe955f0857f90988afca64c
Dewald15 <vdbergdewald@gmail.com>
2024-06-05 14:59:12 +0200

    Create StudentDto and StudentMapper

--- src/main/java/net/sms/mapper/StudentMapper.java
+++ src/main/java/net/sms/mapper/StudentMapper.java
@@ -1,4 +1,25 @@
 package net.sms.mapper;
 
+import net.sms.dto.StudentDto;
+import net.sms.entity.Student;
+
 public class StudentMapper {
+
+    public static StudentDto mapToStudentDto(Student student){
+        return new StudentDto(
+                student.getId(),
+                student.getLastName(),
+                student.getFirstName(),
+                student.getEmail()
+        );
+    }
+
+    public static Student mapToStudent(StudentDto studentDto){
+        return new Student(
+                studentDto.getId(),
+                studentDto.getLastName(),
+                studentDto.getFirstName(),
+                studentDto.getEmail()
+        );
+    }
 }
